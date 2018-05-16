4ce4a248eda5552c125509b88ebdb6c7d487a682
oguzoztoprak <oguzoztoprak@gmail.com>
2018-05-16 22:27:34 +0200

    additional comments and descriptions added

--- Extract_Selection_Mouse.py
+++ Extract_Selection_Mouse.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 13 19:12:41 2018
+### SOFTWARE LAB 2018
+### Phyton-based Paraview sPostProcessor
+### Students: Oguz Oztoprak & Darwin Droll
+### Supervisors: John Jomo & Benjamin Wassermann
+### Disclaimer: Works on our machine, soo.
 
-@author: oguz
-"""
+##' May 2018
 
-from paraview.simple import *
 
+
+### HOW IT WORKS
 
 ## Run this script, with proper source directories, and select some cells or points using the renderview GUI. 
 
@@ -18,9 +19,7 @@ from paraview.simple import *
 ## ShowAndSaveSelection() Shows both the RenderView and SpreadSheetView of the selected points/cells and saves it just like SaveSelection().
 
 
-
-
-
+from paraview.simple import *
 
 def clean_start():
     Disconnect()    
